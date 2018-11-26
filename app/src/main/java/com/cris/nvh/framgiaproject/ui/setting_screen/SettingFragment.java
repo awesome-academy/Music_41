fa38d870a20844633ab641b7d1969a35edf82cd1
hieunv <toiyeuthethao1997@gmail.com>
2018-11-26 13:50:07 +0700

    Load data home screen

---
+++ app/src/main/java/com/cris/nvh/framgiaproject/ui/setting_screen/SettingFragment.java
@@ -0,0 +1,22 @@
+package com.cris.nvh.framgiaproject.ui.setting_screen;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.cris.nvh.framgiaproject.R;
+
+/**
+ * Created by nvh
+ * Contact: toiyeuthethao1997@gmail.com
+ */
+
+public class SettingFragment extends Fragment {
+	@Override
+	public View onCreateView(LayoutInflater inflater,
+	                         ViewGroup container, Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.fragment_setting, container, false);
+	}
+}
